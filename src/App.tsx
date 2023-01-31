70c4c9b9b0a11111b6d34795bcf69a11f0b07d7e
Cathryne Delamort <cathryne.delamort@gmail.com>
2023-01-31 12:55:42 -0500

    Add similar image grouping

--- src/App.tsx
+++ src/App.tsx
@@ -6,44 +6,64 @@ import Image from './Image'
 import FilterPill from './FilterPill'
 import useDebounce from './use-debounce'
 
-type MetaDataKey = keyof typeof images[0]
+type ImageMetadata = typeof images[0]
+type MetaDataKey = keyof ImageMetadata
 type MetaDataLists = Record<MetaDataKey, string[]>
+type ImageGroups = { [key: string]: ImageMetadata[] }
 
-const keys = images.reduce((acc, image) =>
-  (Object.keys(image) as MetaDataKey[]).reduce((data, key) =>
-    !data[key] || data[key].indexOf(image[key]) > -1 
-      ? data
-      : Object.assign(data, { [key]: [...(data[key] || [])].concat(image[key])}),
-    acc
-  ),
-  {} as MetaDataLists 
+const getGroupKey = ({ seed, prompt}: ImageMetadata) => seed + prompt.replace(/\[|\]|\(|\)/g, '')
+const imageGroups = images.reduce(
+  (acc, image) => {
+    const groupKey = getGroupKey(image)
+    acc[groupKey] = acc[groupKey] || []
+    acc[groupKey].push(image)
+    return acc
+  }, 
+  {} as ImageGroups
 )
+const getImageGroup = (image: ImageMetadata) => imageGroups[getGroupKey(image)]
 
 function App() {
-  const [promptFilter, setPromptFilter] = useState('')
+  const [search, setSearch] = useState('')
   const [seedFilter, setSeedFilter] = useState('')
+  const [groupImages, setGroupImages] = useState(false)
   const [samplerFilter, setSamplerFilter] = useState('')
-  const debouncedPromptFilter = useDebounce(promptFilter, 500);
+  const debouncedPromptFilter = useDebounce(search, 500);
   const [filteredImages, setFilteredImages] = useState(images)
   const [sortKey, setSortBy] = useState('-created')
+  const pillFilterApplied = Boolean(seedFilter || samplerFilter)
+
+  const filterImage = ({ seed, prompt, sampler }: ImageMetadata) => 
+    (prompt + seed).match(debouncedPromptFilter) &&
+    (!samplerFilter || sampler === samplerFilter) &&
+    (!seedFilter || seed === seedFilter)
+
+  const getFilteredImageGroup = (image: ImageMetadata) => 
+    getImageGroup(image).filter(filterImage)
+
+  const representsGroup = (image: ImageMetadata) => {
+    const candidates = getFilteredImageGroup(image).sort(sortBy(sortKey))
+    return candidates.length === 1 || image == candidates[0]
+  }
 
   useEffect(() => {
     setFilteredImages(
-      images.filter(
-          ({ prompt, sampler, seed }) => 
-            (prompt + seed).match(debouncedPromptFilter) &&
-            (!samplerFilter || sampler === samplerFilter) &&
-            (!seedFilter || seed === seedFilter)
+      images
+        .filter((image: ImageMetadata) =>
+          filterImage(image) &&
+          (pillFilterApplied || !groupImages || representsGroup(image))
         )
-        .sort(sortBy(`${sortKey}`))
+        .sort(sortBy(sortKey))
     )
-  }, [debouncedPromptFilter, sortKey, seedFilter])
+  }, [debouncedPromptFilter, pillFilterApplied, groupImages, sortKey, seedFilter, imageGroups])
 
   return (
     <div className="App" style={{ display: 'flex', gap: '2rem', flexDirection: 'column' }}>
       <div style={{ display: 'flex', justifyContent: 'center', gap: '1rem' }}>
-        Filter
-        <input type="text" value={promptFilter} onChange={e => setPromptFilter(e.target.value)} />
+        <div style={{ display: 'flex', gap: '.5rem' }}>
+          Filter
+          <input type="text" value={search} onChange={e => setSearch(e.target.value)} />
+        </div>
         <div style={{ display: 'flex', gap: '.5rem' }}>
           Sort by
           <select onChange={e => setSortBy(e.target.value as MetaDataKey)}>
@@ -63,6 +83,12 @@ function App() {
         </div>
       </div>
       <div style={{ display: 'flex', gap: '1rem', justifyContent: 'center' }}>
+        {!pillFilterApplied && 
+            <div style={{ display: 'flex', gap: '.5rem' }} title="Groups images by same seed and prompt">
+              Group similar images
+              <input type="checkbox" checked={groupImages} onChange={e => setGroupImages(e.target.checked)} />
+            </div>
+          }
         {seedFilter &&
           <FilterPill onRemove={() => setSeedFilter('')}>
             🌱 {seedFilter}
