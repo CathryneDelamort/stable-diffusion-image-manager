6fe61bc154576fb2b1a7048500a17fdfd826ee38
Cathryne Delamort <cathryne.delamort@gmail.com>
2023-02-06 09:56:45 -0500

    Update image detail order

--- src/Images/details.ts
+++ src/Images/details.ts
@@ -1,11 +1,12 @@
 const details = {
   model: { title: 'Model hash', prefix: '📦' },
+  prompt: { title: 'Prompt', prefix: '💬' },
+  negativePrompt: { title: 'Negative prompt', prefix: '🤫' },
   seed: { title: 'Seed', prefix: '🌱' },
   cfg: { title: 'CFG value', prefix: 'CFG' },
   denoise: { title: 'Denoise strength', prefix: '🔇' },
   faceRestoration: { title: 'Face restoration', prefix: '🥸' },
   hiresUpscaler: { title: 'Hires upscaler', prefix: '🔎' },
-  prompt: { title: 'Prompt', prefix: '💬' },
   sampler: { title: 'Sampler', prefix: '👀' },
   steps: { title: 'Steps', prefix: '🚶' },
 }
