55e3e0d09a489631d2d4e7e6ec8b2fed7b2c70f6
Cathryne Delamort <cathryne.delamort@gmail.com>
2023-02-14 17:40:04 -0500

    Add swipe right to queue when on review

--- src/Images/SwipeMode.tsx
+++ src/Images/SwipeMode.tsx
@@ -19,13 +19,14 @@ const SwipeMode = ({ onClose }: Props) => {
     moveImages([image], to)
     setSwipeImages(swipeImages.filter(i => JSON.stringify(i) !== JSON.stringify(image)))
   }
+  const { file, folder, prompt } = swipeImages[index]
+  const rightWord = folder == 'review' ? 'Que' : 'Review'
   const handlers = useSwipeable({
     onSwipedLeft: () => handleMove('trash'),
-    onSwipedRight: () => handleMove('review'),
+    onSwipedRight: () => handleMove(rightWord.toLowerCase()),
     onSwipedUp: () => setIndex(index + 1),
     onSwipedDown: () => index > 0 && setIndex(index - 1)
   })
-  const { file, folder, prompt } = swipeImages[index]
   const imgSrc = '/' + ['images', folder, file].filter(f => f).join('/')
 
   return <Stack
@@ -38,7 +39,7 @@ const SwipeMode = ({ onClose }: Props) => {
     <Box position="absolute" style={{ top: '1rem', right: '1rem' }}>
       <button onClick={onClose}>X</button>
     </Box>
-    <Box>← Delete | ↑ Next | ↓ Previous | → Review</Box>
+    <Box>← Delete | ↑ Next | ↓ Previous | {rightWord} →</Box>
     <Box {...handlers}>
       <img src={imgSrc} style={{ maxWidth: '100vw' }} />
     </Box>
